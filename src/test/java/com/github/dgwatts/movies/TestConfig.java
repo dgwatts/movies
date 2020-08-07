c0430307936a588d8439507ff21ccadb2e39f1c7
dgwatts <68703249+dgwatts@users.noreply.github.com>
2020-08-07 15:05:14 +0100

    Add spring autoconfig for tests

---
+++ src/test/java/com/github/dgwatts/movies/TestConfig.java
@@ -0,0 +1,9 @@
+package com.github.dgwatts.movies;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.github.dgwatts.movies")
+public class TestConfig {
+}
