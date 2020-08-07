1228b98a9f4bd9ba5148a9256a20a79fed2be106
dgwatts <68703249+dgwatts@users.noreply.github.com>
2020-08-07 15:17:47 +0100

    Add business logic methods

--- src/main/java/com/github/dgwatts/movies/services/MoviesService.java
+++ src/main/java/com/github/dgwatts/movies/services/MoviesService.java
@@ -1,7 +1,12 @@
 package com.github.dgwatts.movies.services;
 
+import java.util.List;
+
 import com.github.dgwatts.movies.model.Movie;
 import com.github.dgwatts.movies.services.impl.CrudService;
 
 public interface MoviesService extends CrudService<Movie> {
+	List<Movie> findAllByRatingGreaterThan(int rating);
+
+	List<Movie> findAllByDirectorId(int directorId);
 }
